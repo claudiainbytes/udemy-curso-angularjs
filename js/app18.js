71033dc1744f242ee6d9f7800f75e72206b9ff0a
Claudia Estupiñán <Claudia Estupin>
2017-01-26 15:32:13 -0500

    Validacion de formularios

---
+++ js/app18.js
@@ -0,0 +1,9 @@
+angular.module('validacion', [])
+       .controller('MainController', function($scope){
+          
+      });
+      
+
+//Para obtener un modulo definido previamente, contador
+//Retorna un objeto que es el modulo, sin el segundo argumento es decir, sin las dependencias
+angular.module('validacion');
