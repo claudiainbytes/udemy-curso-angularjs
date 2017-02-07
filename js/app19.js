0f12e03e6fdae1111458e7419bfa002e8dc432ab
Claudia Estupiñán <Claudia Estupin>
2017-02-07 10:31:53 -0500

    Enrutamiento

---
+++ js/app19.js
@@ -0,0 +1,9 @@
+angular.module('validacion', ['ngMessages'])
+       .controller('MainController', function($scope){
+          
+      });
+      
+
+//Para obtener un modulo definido previamente, contador
+//Retorna un objeto que es el modulo, sin el segundo argumento es decir, sin las dependencias
+angular.module('validacion');
