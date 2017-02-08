ed6b087322df9fdf2007a115987de215d9b47023
Claudia Estupiñán <Claudia Estupin>
2017-02-08 17:02:34 -0500

    Servicios Parte 1

---
+++ servicios/js/app.js
@@ -0,0 +1,31 @@
+angular.module('enrutamiento', ['ngRoute'])
+      //.factory() recibe dos parametros
+      //primer parametro: nombre del servicio
+      //segundo parametro: funcion del servicio
+      //factory siempre debe retornar un objeto para que sea usado como mi servicio
+      .factory('servicio', function(){
+            //Aqui debemos crear un objeto personalizado
+             var usuarios = ['hugo', 'paco', 'luis'];
+             var miObjeto = {};
+             miObjeto.getUsuarios = function() {
+                  return usuarios;
+             }
+             return miObjeto;     
+      });
+      //.service() recibe dos parametros
+      //primer parametro: nombre del servicio
+      //segundo parametro: funcion del servicio
+      //service se usa la palabra this por que estamos retornando una instancia, la funcion en si es un objeto
+      .service('servicio', function(){
+            //Aqui debemos crear un objeto personalizado
+             var usuarios = ['hugo', 'paco', 'luis'];
+             this.miObjeto = {};
+             this.getUsuarios = function() {
+                  return usuarios;
+             }
+             //no es necesario retornar el objeto     
+      });
+
+
+
+ 
